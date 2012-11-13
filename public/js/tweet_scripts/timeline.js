c6d2ae93fc63183c0871cf94ade4867069f8a46e
Mikael Brevik <mikaelbre@gmail.com>
2012-11-13 10:18:08 +0100

    Provided more comments

--- public/js/tweet_scripts/timeline.js
+++ public/js/tweet_scripts/timeline.js
@@ -2,15 +2,21 @@
   // Create the defaults once
   var pluginName = "twitTimeline",
     defaults = {
+      // Selectors for elements in our module view.
       formSelector: "form",
       tweetListSelector: ".tweet-list",
       refreshButtonSelector: ".refresh-button a",
 
+      // Name of the tweet template 
       tweetTemplate: "tweet",
 
+      // URL to use for fetch
       getURL: "/timeline.json",
+
+      // URL to use for post
       postURL: "/status.json",
 
+      // Standard ajax options. 
       ajaxOptions: {
         count: 50
       }
@@ -23,23 +29,31 @@
     this.options = $.extend( {}, defaults, options) ;        
     this._defaults = defaults;
     this._name = pluginName;
+
+    // Run INIT when constructor is called.
     this.init();
   }
 
   // Extend our plugin to implement the shared methods.
   $.extend(Plugin.prototype, base);
 
+  // Initialize plugin
   Plugin.prototype.init = function () {
+    // Render page structure template 
     this.$el.html(ich.timeline());
+
+    // Show this page. (iplement showPage() base method)
     this.showPage();
     
-
+    // Save different HTML objects
     this.$form = this.$el.find(this.options.formSelector);
     this.$tweetList = this.$el.find(this.options.tweetListSelector);
     this.$refreshButton = this.$el.find(this.options.refreshButtonSelector);
 
+    // bind event listeners
     this.bindEvents();
 
+    // Fetch timeline tweets straight away.
     this.fetch();
   };
 
@@ -78,15 +92,19 @@
   // preventing against multiple instantiations
   $.fn[pluginName] = function ( options ) {
     return this.each(function () {
+      // Is already initiated?
       if ( !$.data(this, "plugin_" + pluginName )) {
+        // No, create instance.
         $.data( this, "plugin_" + pluginName, 
         new Plugin( this, options ));
       } else {
+        // Yes. So we just need to swap page display..
         $.data(this, "plugin_" + pluginName ).showPage();
       }
     });
   };
 
+  // Provide default options to allow people to change them.
   $.fn[pluginName].defaults = defaults;
 
 }));
