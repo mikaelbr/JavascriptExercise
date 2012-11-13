c622daaf2a3fb889234da8642e4c1866e6d5e73c
Mikael Brevik <mikaelbre@gmail.com>
2012-11-13 10:21:05 +0100

    Structure clean up

--- public/js/app.build.js
+++ public/js/app.build.js
@@ -1,17 +1,16 @@
 ({
-  
   baseUrl: '../',
-
   name: 'js/main',
-
   out: "main-built.js",
-  
+
   paths: {
     // Libraries
     text:   'js/_libs/require/text',
     jquery:   'js/_libs/jquery/jquery.182',
     bootstrap: 'js/_libs/bootstrap/bootstrap',
-    
+    ich: 'js/_libs/icanhaz/ICanHaz',
+
+    scripts: 'js/tweet_scripts',
   },
   
   shim: {
